c94884a88b303d4db20f2652ff90d4cdd2fdafed
iamseaniam <7222@holbertonstudents.com>
2024-03-07 15:13:04 -0600

    comments for my understanding

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -6,9 +6,11 @@ This module implements a basic Flask application.
 from flask import Flask
 
 app = Flask(__name__)
+# creates a Flask application in the current Python module
 
 
 @app.route('/', strict_slashes=False)
+# maps the specific URL with the assoiated function.
 def hello():
     """
     A simple view function that returns 'Hello HBNB!'
@@ -17,4 +19,5 @@ def hello():
 
 
 if __name__ == '__main__':
+    # Start the Flask app, accessible from any IP on port 5000
     app.run(host='0.0.0.0', port=5000)
