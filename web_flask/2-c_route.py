a9aea672fdd125e0f4ffa3ab1bd9700338f8398a
iamseaniam <7222@holbertonstudents.com>
2024-03-07 16:19:16 -0600

    added replace method

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -21,8 +21,10 @@ def hbnb():
 @app.route('/c/<text>', strict_slashes=False)
 def c(text):
     """ A simple view function that returns 'C %' """
+    if text in c:
+        text = text[1:-1].replace('_', ' ')
     return 'C %s' % text
 
 
 if __name__ == 'Main':
-    app.run(debig=True, host='0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=5000)
