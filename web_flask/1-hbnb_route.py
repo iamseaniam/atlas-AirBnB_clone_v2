715d68fc9b5bc3058a5be28a3feb8e2fcb26b4fb
iamseaniam <7222@holbertonstudents.com>
2024-03-07 15:30:22 -0600

    question one displaying two things

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+documentation
+"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+app.route('/', strict_slashes=False)
+def hello():
+    """
+    A simple view function that returns 'Hello HBNB!'
+    """
+    return 'Hello HBNB!'
+
+app.route('/hbnb', strict_slashes=False)
+def HBNB():
+    """
+    A simple view function that returns 'HBNB!'
+    """
+    return 'HBNB'
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
