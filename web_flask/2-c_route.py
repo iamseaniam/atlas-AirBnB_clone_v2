f905c8b4dbe1aba1ae4dd58355946e7651d39d67
iamseaniam <7222@holbertonstudents.com>
2024-03-07 16:11:40 -0600

    comments for pep8

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -7,17 +7,22 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
 def hello():
+    """ A simple view function that returns 'Hello HBNB!' """
     return 'Hello HBNB!'
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
+    """ A simple view function that returns 'HBNB!' """
     return 'HBNB'
 
 @app.route('/c/<text>', strict_slashes=False)
 def c(text):
+    """ A simple view function that returns 'C %' """
     return 'C %s' % text
+
 
 if __name__ == 'Main':
     app.run(debig=True, host='0.0.0.0', port=5000)
