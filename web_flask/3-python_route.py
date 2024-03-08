a61ad35f1c05733628bbf0fc7765a1a88c4bd2fa
iamseaniam <7222@holbertonstudents.com>
2024-03-07 18:46:33 -0600

    guessing at pep8 validation

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -8,19 +8,26 @@ app = Flask(__name__)
 
 app.route('/', strict_slashes=False)
 def hello():
+    """comment"""
     return 'Hello HBNB!'
+
 
 app.route('/hbnb', strict_slashes=False)
 def hbnb():
+    """comment"""
     return 'HBNB'
+
 
 app.route('/c/<text>', strict_slashes=False)
 def c(text):
+    """comment"""
     text.replace("_", " ")
     return 'C %' % text
 
+
 app.route('/python/<text>', strict_slashes=False)
 def python(text):
+    """comment"""
     text = 'id cool'
     text.replace("_", " ")
     return 'Python %' % text
