a2576894aa5b6ebbd1a1ad248875110714b60332
iamseaniam <7222@holbertonstudents.com>
2024-03-07 21:11:38 -0600

    question 4

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""comment"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """comment"""
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """comment"""
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c(text):
+    """comment"""
+    text = text.replace("_", " ")
+    return 'C %s' % text
+
+
+@app.route('/python/', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def textPython(text='is cool'):
+    return ('Python ' + text.replace("_", " "))
+
+
+@app.route('/number/<n>', strict_slashes=False)
+def num(num):
+    num = int(num)
+    return '%d is a number' % num
+    
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
