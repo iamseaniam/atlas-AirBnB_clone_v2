a25042bae21eaff73518988400e7cc6e3e5d1fab
iamseaniam <7222@holbertonstudents.com>
2024-03-07 21:22:29 -0600

    pep8

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -8,19 +8,16 @@ app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def hello():
-    """comment"""
     return 'Hello HBNB!'
 
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    """comment"""
     return 'HBNB'
 
 
 @app.route('/c/<text>', strict_slashes=False)
 def c(text):
-    """comment"""
     text = text.replace("_", " ")
     return 'C %s' % text
 
@@ -34,7 +31,7 @@ def textPython(text='is cool'):
 @app.route('/number/<int:n>')
 def is_number(n):
     return f'{n} is a number'
-    
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
