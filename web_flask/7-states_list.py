dbccbf3fc1afeea44023e7cab41f6eaf666539b3
iamseaniam <7222@holbertonstudents.com>
2024-03-12 10:29:55 -0500

    no idea why its not working but reverted last commit

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,22 +1,21 @@
 #!/usr/bin/python3
 """Script for question 7"""
 
-import sys
-sys.path.append(".")
-
-from flask import Flask
-from flask import render_template
-from flask import g
-import logging
+from flask import Flask, render_template
 from models import storage
 
 app = Flask(__name__)
 
 
-@app.route("/states_list", strict_slashes=False)
-def states_route():
-    states = storage.all("State").values()
-    return render_template("7-states_list.html", states=states)
+@app.teardown_appcontext
+def teardown(exception):
+    storage.close()
+
+
+@app.route('/states_list', strict_slashes=False)
+def states_list():
+    states = storage.all("State")
+    return render_template('7-states_list.html', states=states.values())
 
 
 if __name__ == '__main__':
