823cf5340802c42d50c489877208aef95380036f
iamseaniam <7222@holbertonstudents.com>
2024-03-12 10:40:15 -0500

    numberrrrrrr *

---
+++ web_flask/8-cities_by_states.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""Script for question 7"""
+
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+from models.city import City
+
+app = Flask(__name__)
+
+
+@app.route('/cities_by_states', strict_slashes=False)
+def cities_by_states():
+    states = storage.all(State).values()
+    states_sorted = sorted(states, key=lambda x: x.name)
+    for state in states_sorted:
+        if storage.storage_t == "db":
+            state.cities = state.cities
+        else:
+            state.cities = state.cities
+    return render_template('cities_by_states.html', states=states_sorted)
+
+
+@app.teardown_appcontext
+def teardown_db(exception=None):
+    storage.close()
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
