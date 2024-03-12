2f8ec7c40eb5f9e426c366f0f35de8e3020436b5
iamseaniam <7222@holbertonstudents.com>
2024-03-12 09:54:37 -0500

    NUMBER 7

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+""" 7 """
+
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+
+
+app = Flask(__name__)
+
+
+@app.route('/states_list')
+def states_list():
+    states = storage.all(State).values()
+    states_sorted = sorted(states, key=lambda x: x.name)
+    return render_template('7-states_list.html', states=states_sorted)
+
+@app.teardown_appcontext
+def teardown_db(exception=None):
+    storage.close()
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
