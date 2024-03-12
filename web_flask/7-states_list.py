a053dbb9dcdd1098a9724c847d9d9edd899ec933
iamseaniam <7222@holbertonstudents.com>
2024-03-12 10:18:52 -0500

    pep8

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """Script for question 7"""
+
 from flask import Flask, render_template
 from models import storage
 
@@ -15,6 +16,7 @@ def teardown(exception):
 def states_list():
     states = storage.all("State")
     return render_template('7-states_list.html', states=states.values())
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
