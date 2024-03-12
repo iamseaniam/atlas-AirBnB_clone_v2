da44000618af70060cf69f1454e9834dad22c6cf
iamseaniam <7222@holbertonstudents.com>
2024-03-12 10:12:10 -0500

    had list.py 7 complety wrong

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,23 +1,20 @@
 #!/usr/bin/python3
-""" 7 """
-
+"""Script for question 7"""
 from flask import Flask, render_template
 from models import storage
-from models.state import State
-
 
 app = Flask(__name__)
 
-@app.route('/states_list', strict_slashes=False)
-def states_list():
-    states = storage.all(State).values()
-    states_sorted = sorted(states, key=lambda x: x.name)
-    return render_template('states_list.html', states=states_sorted)
 
 @app.teardown_appcontext
-def teardown_db(exception=None):
+def teardown(exception):
     storage.close()
 
+
+@app.route('/states_list', strict_slashes=False)
+def states_list():
+    states = storage.all("State")
+    return render_template('7-states_list.html', states=states.values())
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
